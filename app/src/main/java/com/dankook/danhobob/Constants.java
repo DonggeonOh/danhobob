accaa48108a19414b0946bcf202e67d62ec34d56
서현아 <hyunas1996@gmail.com>
2019-06-27 15:57:38 +0900

    realmserver change

--- app/src/main/java/com/dankook/danhobob/Constants.java
+++ app/src/main/java/com/dankook/danhobob/Constants.java
@@ -1,6 +1,6 @@
 package com.dankook.danhobob;
 
 final class Constants {
-    private static final String INSTANCE_ADDRESS = "https://danhobabopenhack.us1.cloud.realm.io";
+    private static final String INSTANCE_ADDRESS = "https://danhobab.us1.cloud.realm.io";
     static final String AUTH_URL = "https://" + INSTANCE_ADDRESS + "/auth";
 }
