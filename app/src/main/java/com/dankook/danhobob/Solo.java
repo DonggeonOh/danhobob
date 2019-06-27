26184b383ddda6dd32c2a31aa5f71f1af438864e
kddjkdk <giantstar115@naver.com>
2019-06-28 05:36:04 +0900

    add solo

--- app/src/main/java/com/dankook/danhobob/Solo.java
+++ app/src/main/java/com/dankook/danhobob/Solo.java
@@ -3,6 +3,7 @@ package com.dankook.danhobob;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,7 +11,7 @@ import android.widget.EditText;
 public class Solo extends AppCompatActivity {
     private EditText edt;
     private Button btnFood;
-    private
+    boolean []arr = {false, false};  //안눌렸음을 false라고 함
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +20,21 @@ public class Solo extends AppCompatActivity {
     }
 
     public void onRecommend(View v) {
+        R.id.s1
         Intent i = new Intent(this,Recommend.class);
         startActivity(i);
         finish();
     }
 
     public void onCheck(View v){
-
+        btnFood = (Button)v;
+        if(btnFood.getBackground().equals("#ffffff")){
+            Log.d("hi", "onCheck: ");
+            btnFood.setBackgroundColor(0);
+        }
+        else{
+            Log.d("bye", "onCheck200202: ");
+            btnFood.setBackgroundColor(255);
+        }
     }
 }
