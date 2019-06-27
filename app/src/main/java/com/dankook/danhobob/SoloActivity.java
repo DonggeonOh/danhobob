ff306b7e9cd513ecc909560349e68ede58f2ab24
OhDonggeon <cafa3@naver.com>
2019-06-28 05:32:45 +0900

    add solo_pick activity

--- app/src/main/java/com/dankook/danhobob/SoloActivity.java
+++ app/src/main/java/com/dankook/danhobob/SoloActivity.java
@@ -3,13 +3,12 @@ package com.dankook.danhobob;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
 
 public class SoloActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_solo);
+        setContentView(R.layout.activity_solo_pick);
     }
 }
