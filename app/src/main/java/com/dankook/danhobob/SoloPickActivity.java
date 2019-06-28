f6b1476047cc961eee45d46ee28523a8a162eee6
OhDonggeon <cafa3@naver.com>
2019-06-28 13:03:48 +0900

    modified comment

--- app/src/main/java/com/dankook/danhobob/SoloPickActivity.java
+++ app/src/main/java/com/dankook/danhobob/SoloPickActivity.java
@@ -41,32 +41,32 @@ public class SoloPickActivity extends AppCompatActivity {
             case 0:
                 imageViews.setImageResource(R.drawable.food10);
                 foodNames.setText("마라탕");
-                foodExplain.setText("오늘은 날씨가 추우니까 따뜻한 면인 마라탕을 먹는 것을 추천한단호밥!\n\n맛있는 마라탕 먹고 힘내랏!");
+                foodExplain.setText("오늘은 익산에 안개가 꼈네!  기온이 27.4도인 오늘 점심..  따뜻한 중식은 어때? 면인 마라탕을 먹는 것을 추천한단호밥!\n\n매콤하고 맛있는 마라탕 먹고 힘내랏!");
                 break;
             case 1:
                 imageViews.setImageResource(R.drawable.fodod4);
                 foodNames.setText("막국수");
-                foodExplain.setText("오늘은 날씨가 비도 오고 꾸리꾸리하니까, 시원한 막국수 한사발 먹는 것을 추천한단호밥!\n\n막국수 먹고 좋은 하루 보내시길! ٩(◦`꒳´◦)۶");
+                foodExplain.setText("오늘은 익산에 비도 오고 쌀쌀하니까, 부드럽고 담백하고 시원한 막국수 한사발 먹는 것을 추천한단호밥!\n\n막국수 먹고 좋은 하루 보내시길! ٩(◦`꒳´◦)۶");
                 break;
             case 2:
                 imageViews.setImageResource(R.drawable.food2);
                 foodNames.setText("햄버거");
-                foodExplain.setText("오늘은 날씨가 맑고 적당하니까, 식감있는 햄버거 한번 잡솨보라굿!\n나 단호밥이 귀찮을 때 적극 추천한단호밥~_~");
+                foodExplain.setText("오늘의 익산은 날씨가 맑고 적당하네~!무려 27.4도라굿! 식감있는 햄버거 한번 잡솨보라굿!\n나 단호밥이 귀찮을 때 적극 추천한단호밥~_~");
                 break;
             case 3:
                 imageViews.setImageResource(R.drawable.food6);
                 foodNames.setText("초밥");
-                foodExplain.setText("오늘은 바람이 많이 부니까, 살아있는 초밥 한번 먹어보라굿!\n나 단호밥이 정말 좋아한단호밥 ٩(◦`꒳´◦)۶");
+                foodExplain.setText("오늘은 익산에 바람이 많이 부니까, 오랜만에 일식 어때? 차갑게 식감이 살아있는 초밥 한번 먹어보라굿!\n나 단호밥이 정말 좋아한단호밥 ٩(◦`꒳´◦)۶");
                 break;
             case 4:
                 imageViews.setImageResource(R.drawable.food3);
                 foodNames.setText("간장게장");
-                foodExplain.setText("오늘은 바람이 많이 부니까, 짭쪼롬한 간장게장 한번 잡솨보라굿!\n나 단호밥도 먹고싶단호밥+_+");
+                foodExplain.setText("오늘은 익산의 날씨가 27.4도로 밖에 나돌아다니기에 적당한 거 같아,\n 오랜만에 우리 동네 간장게장 맛집을 찾아보는건 어때? 짭쪼롬한 우리의 한식!밥도둑! 간장게장 한번 잡솨보라굿!\n나 단호밥도 먹고싶단호밥+_+");
                 break;
             case 5:
                 imageViews.setImageResource(R.drawable.food1);
                 foodNames.setText("떡볶이");
-                foodExplain.setText("오늘은 날씨가 많이 더우니까, 떡볶이나 한번 잡솨보라굿!\n나 단호밥도 먹고싶단호밥+_+");
+                foodExplain.setText("오늘은 날씨가 27.4도로 밖에 나돌아다니기에 적당한 거 같아 , 떡볶이나 한번 잡솨보라굿!\n나 단호밥도 먹고싶단호밥+_+");
                 break;
         }
     }
